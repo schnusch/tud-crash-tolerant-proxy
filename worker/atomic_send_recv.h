c76d2f8142beba2a4faeb4e10b42957114088556
schnusch <schnusch@users.noreply.github.com>
2026-06-28 01:46:21 +0200

    atomic_send/_recv: doc

--- worker/atomic_send_recv.h
+++ worker/atomic_send_recv.h
@@ -29,13 +29,26 @@ enum {
     ATOMIC_SWAP = 0x7F,
 };
 
+/**
+ * Range of used bytes in `atomic_ring_buffer::buf`.
+ */
 struct ring_buffer_range {
+    /** Offset in `atomic_ring_buffer::buf`. */
     size_t start;
+    /** Number of bytes used in `atomic_ring_buffer::buf`. */
     size_t len;
 };
 
+/**
+ * Number of bytes in `atomic_ring_buffer::buf`.
+ */
 #define RING_BUFFER_SIZE 8192
 
+/**
+ * Ring buffer that can appended to and trimmed atomically. Each buffer has two
+ * `ring_buffer_range`. Operations take place on the inactive range and then
+ * `active` is set atomically to switch the active range.
+ */
 struct atomic_ring_buffer {
     /**
      * State machine around `sendmmsg(2)` and `recvmmsg(2)`.
@@ -60,6 +73,9 @@ struct atomic_ring_buffer {
     char buf[RING_BUFFER_SIZE];
 };
 
+/**
+ * Initialization value of `struct atomic_ring_buffer`.
+ */
 #define ATOMIC_RING_BUFFER_INIT \
     ((const struct atomic_ring_buffer){ \
         .state = ATOMIC_INIT, \
@@ -70,6 +86,11 @@ struct atomic_ring_buffer {
         }, \
     })
 
+/**
+ * Return the currently active `struct ring_buffer_range` of the buffer.
+ * \param b `struct atomic_ring_buffer *`
+ * \return `struct ring_buffer_range *`
+ */
 #define ACTIVE_RANGE(b) (&(b)->ranges[!!(b)->active])
 
 /**
@@ -84,20 +105,24 @@ size_t ring_buffer_append(
 );
 
 /**
- * Drop the first `size` bytes from the ring buffer.
+ * Drop the first `size` bytes from the range.
  */
 void ring_buffer_ltrim(struct ring_buffer_range *range, size_t size);
 
 /**
+ * Send as much data out of `fd` as possible without blocking and trim sent data
+ * from `buf`.
  * \return -1 on error
- * \return >0 the number of bytes sent and removed from `buf`
+ * \return >0 the number of bytes sent and trimmed from `buf`
  */
 int atomic_send(int fd, struct atomic_ring_buffer *buf);
 
 /**
+ * Receive as much data from `fd` as possible without blocking and append to
+ * `buf`.
  * \return -1 on error
  * \return 0 on EOF
- * \return >0 the number of bytes received and added to `buf`
+ * \return >0 the number of bytes received and appended to `buf`
  */
 int atomic_recv(int fd, struct atomic_ring_buffer *buf);
 
