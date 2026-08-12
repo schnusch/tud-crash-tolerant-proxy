386677fb83d383a1f405e65031f7b307ef39a81c
schnusch <schnusch@users.noreply.github.com>
2026-08-13 00:23:50 +0200

    fixup! implement listener process pair

--- listener/main.c
+++ listener/main.c
@@ -1,6 +1,10 @@
 #include "main_passive.h"
 
 int main(int argc, char **argv) {
+#ifdef __has_attribute
+#if __has_attribute(musttail)
     __attribute__((musttail))
+#endif
+#endif
     return main_passive(argc, argv);
 }
