7eb78ec747a4abb9061d2454fb6b6d125ff4a196
schnusch <schnusch@users.noreply.github.com>
2026-08-13 00:14:14 +0200

    fixup! implement listener process pair

--- common/accept.h
+++ common/accept.h
@@ -3,6 +3,10 @@
 
 #include "shared_memory.h"
 
+/**
+ * Allocate a and initialize new `struct connection` in `map` and accept a new
+ * connection on `listen_fd`.
+ */
 struct connection *accept_connection(struct shared_memory_mapping *map, int listen_fd);
 
 #endif
